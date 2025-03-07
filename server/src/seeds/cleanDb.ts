c9120a969ba7909c3a9529489876618d6ecd7ac7
Sara <lokifeathers@gmail.com>
2025-03-07 15:18:30 -0500

    fix: ensure model existence check before listing collections in cleanDb

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,13 +3,15 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    if (models[modelName] && models[modelName].db) {
+    const modelExists = models[modelName].db.db ? await models[modelName].db.db.listCollections({
       name: collectionName
-    }).toArray()
+    }).toArray() : [];
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
+  }
   } catch (err) {
     throw err;
   }
